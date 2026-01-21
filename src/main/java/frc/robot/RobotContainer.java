6359762a9a3ecb28933a3afa2625dca540b1785b
whistlepunkprogram <whistlepunksfrc@gmail.com>
2026-01-21 14:54:33 -0600

    Trying to clean up GIT with clean backup code.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -87,9 +86,9 @@ public class RobotContainer {
     }
 
     // Set up auto commands
-     NamedCommands.registerCommand(
-         "autoIntakeShooterCommand", m_IntakeShooterSubsystem.autoIntakeShooterCommand());
-     NamedCommands.registerCommand("autoFeederCommand", m_FeederSubsystem.autoFeederCommand());
+    NamedCommands.registerCommand(
+        "autoIntakeShooterCommand", m_IntakeShooterSubsystem.autoIntakeShooterCommand());
+    NamedCommands.registerCommand("autoFeederCommand", m_FeederSubsystem.autoFeederCommand());
 
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
@@ -156,22 +155,22 @@ public class RobotContainer {
     // Operator section for controller number two.
 
     // Intake and Spool shooter with right trigger
- m_operatorController
-         .rightTrigger()
-          .whileTrue(m_IntakeShooterSubsystem.runIntakeShooterCommand())
+    m_operatorController
+        .rightTrigger()
+        .whileTrue(m_IntakeShooterSubsystem.runIntakeShooterCommand())
         .onFalse(m_IntakeShooterSubsystem.stopIntakeShooterCommand());
 
     // Run feeder to the shooter while holding right bumper, must hold right trigger too.
-     m_operatorController
-      .rightBumper()
-      .whileTrue(m_FeederSubsystem.runFeederCommand())
-      .onFalse(m_FeederSubsystem.stopFeederCommand());
+    m_operatorController
+        .rightBumper()
+        .whileTrue(m_FeederSubsystem.runFeederCommand())
+        .onFalse(m_FeederSubsystem.stopFeederCommand());
 
     // Outtake and spit out fuel to floor while holding Left Trigger button
-     m_operatorController
-      .leftTrigger()
-     .whileTrue(m_FeederSubsystem.reverseFeederCommand())
-     .onFalse(m_FeederSubsystem.stopFeederCommand());
+    m_operatorController
+        .leftTrigger()
+        .whileTrue(m_FeederSubsystem.reverseFeederCommand())
+        .onFalse(m_FeederSubsystem.stopFeederCommand());
   }
 
   /**
