f4a6f79fbccf62645460e970c8804075750a2a76
whistlepunksprogram <whistlepunksfrc@gmail.com>
2026-02-22 12:56:10 -0600

    This is the code for after week zero.  Added unjam, and climber subsystem but all untested.

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,264 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.commands.DriveCommands;
+import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.subsystems.IntakeShooterSubsystem;
+import frc.robot.subsystems.LeftClimberSubsystem;
+import frc.robot.subsystems.RightClimberSubsystem;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOSpark;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  // Subsystems
+  private final Drive drive;
+  private final FeederSubsystem m_FeederSubsystem = new FeederSubsystem();
+  private final IntakeShooterSubsystem m_IntakeShooterSubsystem = new IntakeShooterSubsystem();
+  private final LeftClimberSubsystem m_LeftClimberSubsystem = new LeftClimberSubsystem();
+  private final RightClimberSubsystem m_RightClimberSubsystem = new RightClimberSubsystem();
+
+  // Controller
+  private final CommandXboxController m_driverController = new CommandXboxController(0);
+  private final CommandXboxController m_operatorController = new CommandXboxController(1);
+
+  // Dashboard inputs
+  private final LoggedDashboardChooser<Command> autoChooser;
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    switch (Constants.currentMode) {
+      case REAL:
+        // Real robot, instantiate hardware IO implementations
+        drive =
+            new Drive(
+                new GyroIOPigeon2(),
+                new ModuleIOSpark(0),
+                new ModuleIOSpark(1),
+                new ModuleIOSpark(2),
+                new ModuleIOSpark(3));
+        break;
+
+      case SIM:
+        // Sim robot, instantiate physics sim IO implementations
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim());
+        break;
+
+      default:
+        // Replayed robot, disable IO implementations
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {});
+        break;
+    }
+
+    // Set up auto commands
+    NamedCommands.registerCommand(
+        "autoIntake",
+        Commands.parallel(
+            m_IntakeShooterSubsystem.autoSlowIntakeCommand(),
+            m_FeederSubsystem.autoReverseFeederCommand()));
+    NamedCommands.registerCommand(
+        "autoShoot",
+        Commands.parallel(
+            m_IntakeShooterSubsystem.autoIntakeShooterCommand(),
+            m_FeederSubsystem.autoFeederCommand()));
+    NamedCommands.registerCommand(
+        "autoOutake",
+        Commands.parallel(
+            m_IntakeShooterSubsystem.autoReverseIntakeShooterCommand(),
+            m_FeederSubsystem.autoFeederCommand()));
+
+    // Register individual commands for backward compatibility with PathPlanner
+    NamedCommands.registerCommand(
+        "autoIntakeShooterCommand", m_IntakeShooterSubsystem.autoIntakeShooterCommand());
+    NamedCommands.registerCommand("autoFeederCommand", m_FeederSubsystem.autoFeederCommand());
+
+    // Set up auto routines
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+
+    // Set up SysId routines
+    autoChooser.addOption(
+        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+    autoChooser.addOption(
+        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+    autoChooser.addOption(
+        "Drive SysId (Quasistatic Forward)",
+        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption(
+        "Drive SysId (Quasistatic Reverse)",
+        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    autoChooser.addOption(
+        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    autoChooser.addOption(
+        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // Default command, normal field-relative drive
+    drive.setDefaultCommand(
+        DriveCommands.joystickDrive(
+            drive,
+            () -> -m_driverController.getLeftY(),
+            () -> -m_driverController.getLeftX(),
+            () -> m_driverController.getRightX())); // Inverted steering (removed negative sign)
+
+    // Lock to 0° when A button is held
+    m_driverController
+        .a()
+        .whileTrue(
+            DriveCommands.joystickDriveAtAngle(
+                drive,
+                () -> -m_driverController.getLeftY(),
+                () -> -m_driverController.getLeftX(),
+                () -> Rotation2d.kZero));
+
+    // Switch to X pattern when X button is pressed
+    m_driverController.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+
+    // Reset gyro to 0° when Y button is pressed
+    m_driverController
+        .y()
+        .onTrue(
+            Commands.runOnce(
+                    () ->
+                        drive.setPose(
+                            new Pose2d(drive.getPose().getTranslation(), Rotation2d.kZero)),
+                    drive)
+                .ignoringDisable(true));
+
+    // Operator section for controller number two.
+
+    // Outtake and spit out fuel to floor while holding right bumper button
+    m_operatorController
+        .rightBumper()
+        .onTrue(
+            new ParallelCommandGroup(
+                m_IntakeShooterSubsystem.runSlowIntakeCommand(),
+                m_FeederSubsystem.reverseFeederCommand()))
+        .onFalse(
+            Commands.parallel(
+                m_IntakeShooterSubsystem.stopIntakeShooterCommand(),
+                m_FeederSubsystem.stopFeederCommand()));
+
+    // Spool up shooter and run intake to SHOOT fuel while holding RIGHT Trigger button
+    // Small delay added to prevent the shooter from stalling when the trigger is first pressed,
+    // which can cause jams.
+    m_operatorController
+        .rightTrigger()
+        .onTrue(
+            new ParallelCommandGroup(
+                m_IntakeShooterSubsystem.runIntakeShooterCommand(),
+                Commands.waitSeconds(0.2).andThen(m_FeederSubsystem.reverseFeederCommand())))
+        .onFalse(
+            Commands.parallel(
+                m_FeederSubsystem.stopFeederCommand(),
+                Commands.waitSeconds(0.2)
+                    .andThen(m_IntakeShooterSubsystem.stopIntakeShooterCommand())));
+
+    // Intake and spin feeder to load FUEL while holding LEFT Trigger button
+    m_operatorController
+        .leftTrigger()
+        .onTrue(
+            new ParallelCommandGroup(
+                m_IntakeShooterSubsystem.reverseIntakeShooterCommand(),
+                m_FeederSubsystem.runFeederCommand()))
+        .onFalse(
+            Commands.parallel(
+                m_IntakeShooterSubsystem.stopIntakeShooterCommand(),
+                m_FeederSubsystem.stopFeederCommand()));
+
+    // Attempt to unjam shooter by reversing the motor.
+    m_operatorController
+        .leftBumper()
+        .onTrue(
+            new ParallelCommandGroup(
+                m_IntakeShooterSubsystem.runUnjamShooterCommand(),
+                m_FeederSubsystem.runFeederCommand()))
+        .onFalse(
+            Commands.parallel(
+                m_IntakeShooterSubsystem.stopIntakeShooterCommand(),
+                m_FeederSubsystem.stopFeederCommand()));
+
+    // Climber controls: left stick up/down drives both climbers in parallel.
+    double climberDeadband = 0.2;
+    Command climberUp =
+        Commands.parallel(
+            m_LeftClimberSubsystem.runLeftClimberCommand(),
+            m_RightClimberSubsystem.runRightClimberCommand());
+    Command climberDown =
+        Commands.parallel(
+            m_LeftClimberSubsystem.reverseLeftClimberCommand(),
+            m_RightClimberSubsystem.reverseRightClimberCommand());
+    Command climberStop =
+        Commands.parallel(
+            m_LeftClimberSubsystem.stopLeftClimberCommand(),
+            m_RightClimberSubsystem.stopRightClimberCommand());
+
+    m_operatorController
+        .axisLessThan(XboxController.Axis.kLeftY.value, -climberDeadband)
+        .whileTrue(climberUp)
+        .onFalse(climberStop);
+
+    m_operatorController
+        .axisGreaterThan(XboxController.Axis.kLeftY.value, climberDeadband)
+        .whileTrue(climberDown)
+        .onFalse(climberStop);
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return autoChooser.get();
+  }
+}
